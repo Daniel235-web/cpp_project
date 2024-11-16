35851b42c925683fb2ed2dea86b821d6efb6946d
Daniel235-web <samueledaniel235@gmail.com>
2024-11-16 18:44:33 +0100

    operators

--- operators.cpp
+++ operators.cpp
@@ -23,7 +23,7 @@ int main () {
    b = a++;
    cout<<a<<endl<<b<<endl;
 
-*/
+
 //relational operations 
 // but here we can use expressions and the operators have to be in brackets for example (a + 5) < (y +2)
 int a = 5, y = 10;
@@ -34,6 +34,46 @@ cout<<"a <= y is: "<<(a <= y)<<endl;
 cout<<"a >= y is: "<<(a >= y )<< endl;
 cout<<"a != y is: "<<(a != y) <<endl;
 cout<<"a == y is: "<<(a == y) <<endl;
+
+//logical operators
+int x = 5, y = 10;
+if((x>5) && (y<15)) {
+    cout<<"x+y"<<(x+y)<<endl;
+
+}else if ((x <= 5) || (y > 15)){
+    cout<<"x-y"<<(x-y)<<endl;
+
+}else{
+    cout <<"!x"<<!x<<endl;
+}
+
+//bitwise operator : always remember to use the brackets in logical and bitwise operators to avoid errors
+
+
+int x = 26;
+int y = 3;
+
+cout<<"x&y:"<<(x&y)<<endl;
+cout<<"x|y"<<(x|y)<<endl;
+cout<<"x^y"<<(x^y)<<endl;
+cout<<"<<"<<(x<<y)<<endl;
+cout<<">>"<<(x>>y)<<endl;
+cout<<"~x"<<(~x)<<endl;
+*/
+
+//miscellaneous operators
+//float a = 2.13;
+//cout<<(int)a<<endl;
+//cout<<a<<endl;
+
+//conditional operators
+int a = 3 ,b = 7;
+
+cout<<"number  is:"<<((a < b) ? (a + 2) : b)<<endl;
+
+return 0;
+
+
 
    
 }
