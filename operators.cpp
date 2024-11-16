50161d8f629c4a7f1ae2a77796d0e0fa394c59af
Daniel235-web <samueledaniel235@gmail.com>
2024-11-16 15:14:52 +0100

    arithmetic and relational operators

---
+++ operators.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+//arithemetic operators
+
+int main () {
+    /*int a = 3, b = 10;
+    cout<<"addition " <<a+b<<endl;
+    cout<<"subtraction "<<a-b<<endl;
+    cout<<"division "<<a/b<<endl;
+    cout<<"multiplication "<<a*b<<endl;
+    cout<<"modul "<<a%b<<endl;
+    
+
+   // post and pre increment operators
+   //here we can't use expressions like b = (a + 5) or b = ++5 it will give error
+
+   int a = 10, b;
+    b= ++a;
+  
+ cout<<a<<endl<<b<<endl;
+   
+   b = a++;
+   cout<<a<<endl<<b<<endl;
+
+*/
+//relational operations 
+// but here we can use expressions and the operators have to be in brackets for example (a + 5) < (y +2)
+int a = 5, y = 10;
+
+cout<<"a < y is: "<<((a + 5) < (y +2))<<endl;
+cout<<"a > y is: "<<(a > y)<<endl;
+cout<<"a <= y is: "<<(a <= y)<<endl;
+cout<<"a >= y is: "<<(a >= y )<< endl;
+cout<<"a != y is: "<<(a != y) <<endl;
+cout<<"a == y is: "<<(a == y) <<endl;
+
+   
+}
