e0617f1c33966ac97e8a70dd41fc19500f8b1e1c
Daniel235-web <samueledaniel235@gmail.com>
2024-11-21 22:52:51 +0100

    forloops

---
+++ forloop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int i,n, sum = 0;
+
+int main(){
+
+ for (i = 0; i <= 10; i++){
+   sum = sum + i;
+   
+ }
+cout<<"the sum of number is :"<<sum<<endl;
+
+ };
