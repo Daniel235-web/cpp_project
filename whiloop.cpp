c86fa7dbed4c8a382d2da97bd9f17a125d2b09ba
Daniel235-web <samueledaniel235@gmail.com>
2024-11-22 16:30:17 +0100

    while loop

---
+++ whiloop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+ cout<<"please enter a number greater than 6"<<endl;
+ cin>>n;
+  while(n <=6){
+    cout<<"please enter a valid number"<<endl;
+    cin>>n;
+
+  }
+  cout<<"thanks for inputing the valid number"<<endl;
+}
