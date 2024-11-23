4306519ac46f25d16662171feffb8373335fc713
Daniel235-web <samueledaniel235@gmail.com>
2024-11-23 14:40:02 +0100

    do while loops

---
+++ dowhileloop.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+#define PI 3.142;
+
+int main(){
+  /*int i = 0;
+
+    do {
+        cout<<"daniel how are you doing"<<endl;
+
+    }while(i > 0);
+    cout<<"out of loop"<<endl;
+    return 0;
+    
+
+   int number ;
+   cout<<"enter number greater than 5:"<<endl;
+   cin>>number;
+
+   while(number <= 5){
+     cout<<"input valid number:"<<endl;
+     cin>>number ;
+   }
+   cout<<"thanks"<<endl;
+   
+  //using do while loop for input validation;
+
+  int number;
+  
+  do {
+  cout<<"enter a number greater than 5"<<endl;
+  cin>>number;
+
+  }while(number <= 5);
+  */
+
+ //to calculate the area of a circle
+
+
+
+
+
+}
