81c2320991a9a66a02de44a5ccaaab19118c29ba
Daniel235-web <samueledaniel235@gmail.com>
2024-11-23 12:08:37 +0100

    while loop

---
+++ whileloop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cout<<"enter the number you want"<<endl;
+    cin>>number;
+    while (number) {
+        cout<<"the number is: "<<number<<endl;
+        number++;
+    };
+};
--- whiloop.cpp
+++ whiloop.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int n;
+ /*   int n;
  cout<<"please enter a number greater than 6"<<endl;
  cin>>n;
   while(n <=6){
@@ -11,4 +11,42 @@ int main() {
 
   }
   cout<<"thanks for inputing the valid number"<<endl;
+
+  //using for loops for validation
+
+  int a;
+  cout<<"please enter a number greater than 6"<<endl;
+  cin>>a;
+  for( ; a<=6; ){
+    cout<<"enter a valid number"<<endl;
+    cin>>a;
+  }
+  cout<<"thanks for inputing the right number"<<endl;
+  */
+ //using flag
+
+ int b;
+bool check = 0;
+ 
+
+ while (!check){
+    cout<<"enter a number between 2 and 7"<<endl;
+    cin>>b;
+    if (b<=2 || b>=7){
+        cout<<"enter a valid number"<<endl;
+
+    }
+    else{
+       cout<<"thanks for entering the right number"<<endl;
+       check= 1;
+    };
+    
+ }
+ 
+return 0; 
+
+
 }
+
+
+
