da90692c87f462b0a225b5411dc131c442c916d6
Daniel235-web <samueledaniel235@gmail.com>
2024-11-24 13:14:54 +0100

    continue statement

---
+++ continueTutorial.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+   /*  int i;
+    for (i = 0 ; i<=10; i++){
+        if (i == 7){
+            continue;
+        };
+        cout<<i<<endl;
+    };
+    
+
+   int i, num, total= 0;
+
+   for (i = 1; i<=10; i++){
+    cout<<"enter a number to add"<<endl;
+    cin>>num;
+    if (num < 0){
+        continue;
+    }
+    total += num;
+   }
+   
+   cout <<"the total is:"<<total<<endl;
+   return 0;
+   */
+};
