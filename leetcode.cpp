76540ba9fe2d17f32d7451905016ebaa1e1117ec
Daniel235-web <samueledaniel235@gmail.com>
2024-11-25 15:30:15 +0100

    leetcode2

--- leetcode.cpp
+++ leetcode.cpp
@@ -5,7 +5,7 @@ int main (){
    /*  int number[15]={0};
     number[0] = 10;
     number[14] = 150;
-    */
+
     int sum[5],i;
    int firstnumbers[5] = {1,2,12,3,4};
    int secondnumbers[5] = {9,8,3,1,2};
@@ -18,7 +18,21 @@ int main (){
      cout<<sum[i]<<endl;
    }
     return 0;
+    */
+    int sum , i,j;
+   int nums[] = {2,7,11,15};
 
+    for (i = 0; i < 4; i++){
+        for (j = i+1; j< 4; j++){
+            if (nums[i] + nums[j] == 9){
+                cout<<i<<j<<endl;
+            }
+        }
+
+    }
+
+
+ return 0;
 
    
 }
